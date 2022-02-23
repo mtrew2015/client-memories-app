715d8dbb188fd1f2363517f0a92d91a744e4cb26
Michael Trew <mtrew2015@gmail.com>
2022-02-23 14:08:01 -0500

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'http://memories-project-50.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,13 +5,13 @@ import useStyles from './styles';
 import {useSelector} from 'react-redux'
 
 const Posts = ({setCurrentId}) => {
-  const posts = useSelector((state) => state.posts)
+  const posts = useSelector((state) => state.posts) || []
   console.log(posts)
   const classes = useStyles();
   return (
     !posts.length ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
+        {posts && posts.map((post) => (
           <Grid item key={post._id} xs={12} sm={6}>
             <Post setCurrentId={setCurrentId} post={post} />
           </Grid>
